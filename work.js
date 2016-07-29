04abfa6b71f04cea38d15a90ab443d96a74464d9
christiannicolasf <christiannicolasfeidner@gmail.com>
2016-07-29 12:31:06 +0200

    Create work.js

---
+++ work.js
@@ -0,0 +1,31 @@
+
+var works = [
+	{
+		title: "First Pillar",
+		pic: "img/html.png",
+		link: "https://en.wikipedia.org/wiki/HTML",
+		text: "The first pillar of coding. By building the basic structure your future success is being garanteed.",
+		buttontitle: "HTML"
+	},
+	{
+		title: "Second Pillar",
+		pic: "img/css.png",
+		link: "https://en.wikipedia.org/wiki/CSS_(disambiguation)",
+		text: "The second pillar of excellent web-development. By using CSS the basic structure finally is given style.",
+		buttontitle: "CSS"
+	},
+	{
+		title: "Third Pillar",
+		pic: "img/javascript.png",
+		link: "https://en.wikipedia.org/wiki/JavaScript",
+		text: "The third pillar giving your website an experience. Java enables first steps into making the website interactive.",
+		buttontitle: "Javascript"
+	},
+	{
+		title: "Fourth Pillar",
+		pic: "img/ror.png",
+		link: "https://en.wikipedia.org/wiki/Ruby_on_Rails",
+		text: "The fourth pillar of basic coding enables you to build an effective back-end for your website.",
+		buttontitle: "Ruby on Rails"
+	}
+];
